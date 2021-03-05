ca106b6918fd955d7da6388fc64c9a908616daff
luke <lukebraun88@att.net>
2021-03-05 11:14:51 -0600

    redid migrations for heroku

--- app/models/user.py
+++ app/models/user.py
@@ -2,42 +2,41 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  name = db.Column(db.String(40), nullable = False)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
-  created_at = db.Column(db.DateTime, server_default=db.func.now())
-  updated_at = db.Column(db.DateTime, server_default=db.func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
 
-  _body_parts = db.relationship(
+    _body_parts = db.relationship(
         "BodyPart", backref="users", cascade="all, delete-orphan")
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @property
+    def password(self):
+        return self.hashed_password
 
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
+    @property
+    def body_parts(self):
+        return [x.to_dict() for x in self._body_parts]
 
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-  @property
-  def body_parts(self):
-      return [x.to_dict() for x in self._body_parts]
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "name": self.name,
-      "bodyParts":self.body_parts,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "name": self.name,
+            "bodyParts": self.body_parts,
+            "email": self.email
+        }
