087c5627e10f331b3fbbc6bdd4085bcb7b993795
luke <lukebraun88@att.net>
2021-03-09 16:50:15 -0600

    create stat form decent css

--- react-app/src/components/CreateLift.js
+++ react-app/src/components/CreateLift.js
@@ -9,9 +9,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import purple from '@material-ui/core/colors/purple';
-
-
 
     const theme = createMuiTheme({
         palette: {
@@ -36,8 +33,6 @@ import purple from '@material-ui/core/colors/purple';
                 root:{
                     fontWeight:400,
                     fontSize: 20,
-                    // fontFamily: 'sans-serif',
-                    // fontSize: 20
                 }
             }
 
@@ -45,10 +40,8 @@ import purple from '@material-ui/core/colors/purple';
     });
 
 
-
-const buttonStyle1 = {
+const buttonStyle = {
     // background: 'linear-gradient(45deg, #34c0b9 30%, #f14d8a 90%)',
-    // background:'#e8e8e8',
     borderRadius: 10,
     border: 0,
     color: 'black',
@@ -58,19 +51,6 @@ const buttonStyle1 = {
     padding: '0 30px',
     // boxShadow: '0 3px 5px 2px #34c0b9',
 }
-const buttonStyle2 = {
-    // background: 'linear-gradient(45deg, #34c0b9 30%, #f14d8a 90%)',
-    borderRadius: 10,
-    border: 0,
-    fontSize: 16,
-    fontWeight: 400,
-    color: 'black',
-    height: 55,
-    padding: '0 30px',
-    // boxShadow: '0 3px 5px 2px #34c0b9',
-}
-
-
 
 const inputStyle = {
     width: 240,
@@ -130,13 +110,11 @@ const CreateLift = ({ authenticated }) => {
 
     return (
         <div className="body" onClick={() => closeMenu()}>
-            <div className="stat-create_container">
+            <div className="lift-create_container">
                 <p className="table-heading">CREATE LIFT</p>
                 <ThemeProvider theme={theme}>
-                <form className="stat-create_form" noValidate autoComplete="off" onSubmit={onCreateLift}>
-
-                    <div className="stat-create_inputs">
-                        {/* <label for="title">TITLE</label> */}
+                <form className="lift-create_form" noValidate autoComplete="off" onSubmit={onCreateLift}>
+                    <div className="lift-create_inputs">
                         <div>
                             <TextField
                                 id="title"
@@ -144,24 +122,11 @@ const CreateLift = ({ authenticated }) => {
                                 label="TITLE"
                                 name="title"
                                 type="text"
-                                    // InputLabelProps={{
-                                    //     classes: {
-                                    //         root: classes.cssLabel,
-                                    //         focused: classes.cssFocused,
-                                    //     },
-                                    // }}
-                                // className={classes.root}
-                                // InputLabelProps={{
-                                //     FormLabelClasses: {
-                                //         className: classes.overrides
-                                //     },
-                                // }}
                                 value={title}
-                                    style={inputStyle}
+                                style={inputStyle}
                                 onChange={(e) => setTitle(e.target.value)}
                             />
                         </div>
-                        {/* <label for="description">DESCRIPTION</label> */}
                         <div>
                             <TextField
                                 multiline
@@ -177,10 +142,9 @@ const CreateLift = ({ authenticated }) => {
                                 onChange={(e) => setDescription(e.target.value)}
                             />
                         </div>
-                        {/* <label for="bodyPart"></label> */}
                         <div>
                             <TextField
-                                id="outlined-select"
+                                id="bodypart_select"
                                 select
                                 name="bodyPart"
                                 label="BODY PART"
@@ -200,11 +164,11 @@ const CreateLift = ({ authenticated }) => {
 
                     </div>
 
-                    <div className="stat-create_buttons">
-                        <Button style={buttonStyle1} variant="contained" className="stat-back_button" type="button" onClick={() => goBack()}>Back</Button>
-                        <Button style={buttonStyle2} variant="contained"className="stat-create_button" type="submit">Create</Button>
+                    <div className="lift-create_buttons">
+                        <Button style={buttonStyle} variant="contained" className="lift-back_button" type="button" onClick={() => goBack()}>Back</Button>
+                        <Button style={buttonStyle} variant="contained"className="lift-create_button" type="submit">Create</Button>
                     </div>
-                    <div className="stat-create_errors">
+                    <div className="lift-create_errors">
                         {/* {errors.map((error) => (
                             <div>{error}</div>
                         ))} */}
--- react-app/src/components/CreateStat.js
+++ react-app/src/components/CreateStat.js
@@ -3,16 +3,22 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Redirect, useHistory } from "react-router-dom";
 import * as liftActions from "../store/reducers/lifts"
 import * as sessionActions from "../store/reducers/session"
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const CreateStat = ({ authenticated }) => {
     const [errors, setErrors] = useState([])
-    const [sets, setSets] = useState(0);
-    let [reps, setReps] = useState(0);
-    let [weight, setWeight] = useState(0);
-    let [date, setDate] = useState("")
+    const [sets, setSets] = useState();
+    let [reps, setReps] = useState();
+    let [weight, setWeight] = useState();
+    let [date, setDate] = useState()
     let [difficulty, setDifficulty] = useState("")
     let [notes, setNotes] = useState("")
-
 
     const user = useSelector((x) => x.session.user)
     const lift = useSelector((x) => (x.shownLifts.lift))
@@ -28,107 +34,184 @@ const CreateStat = ({ authenticated }) => {
         history.push("/show-lift")
     }
 
-
-
     const onCreateStat = async (e) => {
         e.preventDefault();
         try {
-            await dispatch(liftActions.createStat({ sets, reps, weight, date, difficulty, notes, liftId:lift.id }))
-            await dispatch(liftActions.setShownLifts({liftId: lift.id}))
+            await dispatch(liftActions.createStat({ sets, reps, weight, date, difficulty, notes, liftId: lift.id }))
+            await dispatch(liftActions.setShownLifts({ liftId: lift.id }))
             await history.push("/show-lift")
-
         } catch (err) {
             setErrors(err);
         }
     };
 
+    const buttonStyle = {
+        // background: 'linear-gradient(45deg, #34c0b9 30%, #f14d8a 90%)',
+        borderRadius: 10,
+        border: 0,
+        color: 'black',
+        fontSize: 16,
+        fontWeight: 400,
+        height: 60,
+        padding: '0 30px',
+        // boxShadow: '0 3px 5px 2px #34c0b9',
+    }
+
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                main: '#34c0b9',
+            },
+            secondary: {
+                main: '#ffffff',
+            },
+        },
+        overrides: {
+            MuiInputLabel: {
+                root: {
+                    color: "black",
+                    "&$focused": {
+                        color: "#34c0b9",
+                        fontWeight: 600,
+                    },
+                }
+            },
+            MuiFilledInput: {
+                root: {
+                    fontWeight: 400,
+                    fontSize: 20,
+                }
+            }
+
+        }
+    });
+
+    const inputStyle = {
+        width: 240,
+        background: 'linear-gradient(45deg, white 30%, white 90%)',
+        borderRadius: 10,
+        border: 0,
+        fontSize: 30,
+        fontWeight: 400,
+        fontFamily: 'sans-serif',
+        color: 'black',
+        display: 'flex',
+        textAlign: 'left',
+        // height: 48,
+        // padding: '0 30px',
+        // boxShadow: '0 3px 2px 2px white',
+    }
+
+
 
     return (
         <div className="body" onClick={() => closeMenu()}>
-        <div className="stat-create_container">
+            <div className="stat-create_container">
                 <p className="table-heading">{lift ? lift.title : "Example Heading"}</p>
-            <form className="stat-create_form" onSubmit={onCreateStat}>
+                <ThemeProvider theme={theme}>
+                    <form className="stat-create_form" onSubmit={onCreateStat}>
 
-                {/* <label htmlFor="email">Email</label> */}
-                <div className="stat-create_inputs">
-                    <label for="date">DATE</label>
-                    <div>
-                        <input
-                            name="date"
-                            type="date"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)}
-                        />
-                    </div>
-                    <label for="sets">SETS</label>
-                    <div>
-                        <input
-                            name="sets"
-                            type="number"
-                            placeholder="sets"
-                            value={sets}
-                            onChange={(e)=>setSets(e.target.value)}
-                        />
-                    </div>
-                    <label for="reps">REPS</label>
-                    <div>
-                        <input
-                            name="reps"
-                            type="number"
-                            placeholder="Reps"
-                            value={reps}
-                            onChange={(e) => setReps(e.target.value)}
-                        />
-                    </div>
-                    <label for="weight">WEIGHT</label>
-                    <div>
-                        <input
-                            name="weight"
-                            type="number"
-                            placeholder="Weight"
-                            value={weight}
-                            onChange={(e) => setWeight(e.target.value)}
-                        />
-                    </div>
-                    <label for="difficulty">DIFFICULTY</label>
-                    <div>
-                        <select
-                            name="difficulty"
-                            // type="text"
-                            // placeholder="
-                            value={difficulty}
-                            onChange={(e) => setDifficulty(e.target.value)}>
-                            <option disabled="true" value="">------ choose option ------</option>
-                            <option value="easy">easy</option>
-                            <option value="medium">medium</option>
-                            <option value="hard">hard</option>
-                            </select>
-                    </div>
-                    <label for="notes">NOTES</label>
-                    <div>
-                        <input
-                            name="notes"
-                            type="text"
-                            placeholder=""
-                            value={notes}
-                            onChange={(e) => setNotes(e.target.value)}
-                        />
-                    </div>
-                </div>
+                        <div className="stat-create_inputs">
+                            <div className="create-1">
 
-                <div className="stat-create_buttons">
-                    <button className="stat-back_button" type="button" onClick={()=>goBack()}>Back</button>
-                    <button className="stat-create_button" type="submit">Create</button>
-                </div>
-                <div className="stat-create_errors">
-                    {errors.map((error) => (
-                        <div>{error}</div>
-                    ))}
 
-                </div>
-            </form>
+                                <TextField
+                                    label="SETS"
+                                    variant="filled"
+                                    style={inputStyle}
+                                    name="sets"
+                                    type="number"
+                                    placeholder="sets"
+                                    value={sets}
+                                    onChange={(e) => setSets(e.target.value)}
+                                />
 
-        </div>
+
+                                <TextField
+                                    label="REPS"
+                                    variant="filled"
+                                    style={inputStyle}
+                                    name="reps"
+                                    type="number"
+                                    placeholder="reps"
+                                    value={reps}
+                                    onChange={(e) => setReps(e.target.value)}
+                                />
+
+
+                                <TextField
+                                    label="WEIGHT"
+                                    variant="filled"
+                                    style={inputStyle}
+                                    name="weight"
+                                    type="number"
+                                    placeholder="weight"
+                                    value={weight}
+                                    InputProps={{
+                                        endAdornment: <InputAdornment position="end">lb</InputAdornment>,
+                                    }}
+                                    onChange={(e) => setWeight(e.target.value)}
+                                />
+                                <TextField
+                                    id="difficulty_select"
+                                    name="difficulty"
+                                    select
+                                    label="DIFFICULTY"
+                                    value={difficulty}
+                                    variant="filled"
+                                    style={inputStyle}
+                                    onChange={(e) => setDifficulty(e.target.value)}>
+                                    <MenuItem disabled="true" value="">------ choose MenuItem ------</MenuItem>
+                                    <MenuItem value="easy">easy</MenuItem>
+                                    <MenuItem value="medium">medium</MenuItem>
+                                    <MenuItem value="hard">hard</MenuItem>
+                                </TextField>
+                            </div>
+
+                            <div className="create-2">
+                                <TextField
+                                    label="DATE"
+                                    variant="filled"
+                                    style={inputStyle}
+                                    name="date"
+                                    type="date"
+                                    value={date}
+                                    onChange={(e) => setDate(e.target.value)}
+                                />
+
+
+
+
+
+                                <TextField
+                                    multiline
+                                    id="notes"
+                                    variant="filled"
+                                    label="NOTES"
+                                    name="notes"
+                                    type="text"
+                                    rowsMax={4}
+                                    style={inputStyle}
+                                    placeholder="notes"
+                                    value={notes}
+                                    onChange={(e) => setNotes(e.target.value)}
+                                />
+                                <div className="stat-create_buttons">
+                                    <Button style={buttonStyle} variant="contained" className="stat-back_button" type="button" onClick={() => goBack()}>Back</Button>
+                                    <Button style={buttonStyle} variant="contained" className="stat-create_button" type="submit">Create</Button>
+                                </div>
+                                </div>
+                            </div>
+
+                        <div className="stat-create_errors">
+                            {errors.map((error) => (
+                                <div>{error}</div>
+                            ))}
+
+                        </div>
+                    </form>
+                </ThemeProvider>
+            </div>
         </div>
     );
 };
